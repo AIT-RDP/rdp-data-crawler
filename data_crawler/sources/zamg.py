012eb690b466a44e78afdea2a35214ec35e553ed
Michael Spiegel <michael.spiegel@ait.ac.at>
2022-11-14 19:23:45 +0100

    feat: ZAMG TAWES Endpoint (#22)

--- data_crawler/sources/zamg.py
+++ data_crawler/sources/zamg.py
@@ -1,6 +1,7 @@
 """
 Implements supported ZAMG data sources
 """
+import dataclasses
 import datetime
 import itertools
 import logging
@@ -17,34 +18,73 @@ class MeasurementStationData(http_cache.GenericHTTPSourceAPI):
     """
     Fetches the measurement station data provided in 10 minute resolution
 
-    A description of the dataset can be found at https://data.hub.zamg.ac.at/dataset/klima-v1-10min . Be aware that the
-    data is only available until midnight, the day before. Hence, the data source aligns all requests to a full day and
-    only updates new days, if needed.
+    The API currently implements the climate and tawes endpoint which mostly differ by their delay and available data
+    points. A description of the climate dataset can be found at https://data.hub.zamg.ac.at/dataset/klima-v1-10min .
+    Be aware that the data is only available until midnight, the day before. Hence, the data source aligns all requests
+    to a full day and only updates new days, if needed. The meta-fields can be accessed at
+    https://dataset.api.hub.zamg.ac.at/v1/station/historical/klima-v1-10min/metadata .
+
+    Similarly, the TAWES meta fields are accessible at
+    https://dataset.api.hub.zamg.ac.at/v1/station/historical/tawes-v1-10min/metadata . Note that both TAWES and climate
+    datasets use different station ids. One can use the meta-endpoint to extract the station ids as well.
     """
 
+    @dataclasses.dataclass
+    class _PRef:
+        """Parameter reference to define some transformation rules"""
+        name: str
+        scaling: float = 1.0
+
     _parameter_mapping = {
-        "TL": "air_temperature_2m",  # [degC]
-        "TP": "dew_point_temperature_2m",  # [degC]
-        "RF": "relative_humidity_2m",  # [%]
+        "climate": {  # Parameter mappings for the climate endpoint
+            "TL": _PRef(name="air_temperature_2m"),  # [degC]
+            "TP": _PRef(name="dew_point_temperature_2m"),  # [degC]
+            "RF": _PRef(name="relative_humidity_2m"),  # [%]
 
-        "P": "air_pressure",  # [hPa]
-        "P0": "air_pressure_at_sea_level",  # [hPa]
+            "P": _PRef(name="air_pressure"),  # [hPa]
+            "P0": _PRef(name="air_pressure_at_sea_level"),  # [hPa]
 
-        "DD": "wind_direction_10m",  # [deg]
-        "DDX": "wind_direction_gust_10m",  # [deg]
-        "FF": "wind_speed_10m",  # [m/s]
-        "FFX": "wind_speed_gust_10m",  # [m/s]
+            "DD": _PRef(name="wind_direction_10m"),  # [deg]
+            "DDX": _PRef(name="wind_direction_gust_10m"),  # [deg]
+            "FF": _PRef(name="wind_speed_10m"),  # [m/s]
+            "FFX": _PRef(name="wind_speed_gust_10m"),  # [m/s]
 
-        "GSX": "global_horizontal_irradiation",  # [W/m^2]
-        "HSX": "diffuse_irradiation",  # [W/m^2]
+            "GSX": _PRef(name="global_horizontal_irradiation"),  # [W/m^2]
+            "HSX": _PRef(name="diffuse_irradiation"),  # [W/m^2]
 
-        "RR": "precipitation_total_10min",  # [mm]
-        "RRM": "precipitation_flag",  # [1]
+            "RR": _PRef(name="precipitation_total_10min"),  # [mm]
+            "RRM": _PRef(name="precipitation_flag"),  # [1]
+            "SH": _PRef(name="snow_depth", scaling=10.0),  # [cm] converted to [mm]
 
-        "TB1": "soil_temperature_10_cm",  # [degC]
-        "TB2": "soil_temperature_20_cm",  # [degC]
-        "TB3": "soil_temperature_50_cm",  # [degC]
-        "TS": "air_temperature_5cm",  # [degC]
+            "TB1": _PRef(name="soil_temperature_10_cm"),  # [degC]
+            "TB2": _PRef(name="soil_temperature_20_cm"),  # [degC]
+            "TB3": _PRef(name="soil_temperature_50_cm"),  # [degC]
+            "TS": _PRef(name="air_temperature_5cm"),  # [degC]
+        },
+        "tawes": {  # Parameter mapping for the tawes endpoint
+            "TL": _PRef(name="air_temperature_2m"),  # [degC]
+            "TP": _PRef(name="dew_point_temperature_2m"),  # [degC]
+            "RFAM": _PRef(name="relative_humidity_2m"),  # [%]
+
+            "P": _PRef(name="air_pressure"),  # [hPa]
+            "PRED": _PRef(name="air_pressure_at_sea_level"),  # [hPa]
+
+            "DD": _PRef(name="wind_direction_10m"),  # [deg]
+            "DDX": _PRef(name="wind_direction_gust_10m"),  # [deg]
+            "FFAM": _PRef(name="wind_speed_10m"),  # [m/s]
+            "FFX": _PRef(name="wind_speed_gust_10m"),  # [m/s]
+
+            "GLOW": _PRef(name="global_horizontal_irradiation"),  # [W/m^2]
+
+            "RR": _PRef(name="precipitation_total_10min"),  # [mm]
+            "RRM": _PRef(name="precipitation_flag"),  # [1]
+            "SCHNEE": _PRef(name="snow_depth", scaling=10.0),  # [cm] converted to [mm]
+
+            "TB1": _PRef(name="soil_temperature_10_cm"),  # [degC]
+            "TB2": _PRef(name="soil_temperature_20_cm"),  # [degC]
+            "TB3": _PRef(name="soil_temperature_50_cm"),  # [degC]
+            "TS": _PRef(name="air_temperature_5cm"),  # [degC]
+        }
     }
 
     def __init__(self, source_parameters, executor_name, **kwargs):
@@ -59,11 +99,15 @@ class MeasurementStationData(http_cache.GenericHTTPSourceAPI):
 
         self._logger = logging.getLogger(f"{__name__}.{self.__class__.__name__}.{executor_name}")
 
-        data_points = list(source_parameters.get("data points", self._parameter_mapping.keys()))
-        invalid_data_points = set(data_points).difference(self._parameter_mapping.keys())
+        endpoint_name = source_parameters.get("endpoint", "climate")
+        self._endpoint_url = self._resolve_endpoint(endpoint_name)
+        self._local_mapping = self._parameter_mapping[endpoint_name.lower()]
+
+        data_points = list(source_parameters.get("data points", self._local_mapping.keys()))
+        invalid_data_points = set(data_points).difference(self._local_mapping.keys())
         if len(invalid_data_points) > 0:
             raise ValueError(f"Invalid entries in the list of data points: {invalid_data_points}. Only "
-                             f"{list(self._parameter_mapping.keys())} supported")
+                             f"{list(self._local_mapping.keys())} are supported for {endpoint_name}")
 
         self._static_request_parameters = {
             "station_ids": source_parameters["station id"],  # Visible from the web GUI
@@ -72,7 +116,7 @@ class MeasurementStationData(http_cache.GenericHTTPSourceAPI):
             "parameters": data_points
         }
 
-        self._extractors = self._compile_extractors(data_points)
+        self._extractors = self._compile_extractors(data_points, self._local_mapping)
 
         initial_history = pd.to_timedelta(source_parameters.get("initial history", "48h"))
         self._last_query_ts = datetime.datetime.now(tz=datetime.timezone.utc) - initial_history
@@ -80,7 +124,22 @@ class MeasurementStationData(http_cache.GenericHTTPSourceAPI):
         self._drop_missing_observations = source_parameters.get("drop missing observations", False)
 
     @staticmethod
-    def _compile_extractors(data_points: List[str]) -> List[jx.PathExtractor]:
+    def _resolve_endpoint(endpoint_name: str) -> str:
+        """Resolves the endpoint name into an URL"""
+
+        endpoints = {
+            "climate": "https://dataset.api.hub.zamg.ac.at/v1/station/historical/klima-v1-10min",
+            "tawes": "https://dataset.api.hub.zamg.ac.at/v1/station/historical/tawes-v1-10min",
+        }
+
+        endpoint_name = endpoint_name.lower()
+        if endpoint_name not in endpoints:
+            raise ValueError(f"Unknown measurement station endpoint '{endpoint_name}', only "
+                             f"{list(endpoints.keys())} supported.")
+        return endpoints[endpoint_name]
+
+    @staticmethod
+    def _compile_extractors(data_points: List[str], local_mapping: Dict[str, _PRef]) -> List[jx.PathExtractor]:
         """Generates the list of extractors based on the selected data points"""
 
         # Static features:
@@ -92,11 +151,13 @@ class MeasurementStationData(http_cache.GenericHTTPSourceAPI):
         ]
 
         # Dynamically selected features
-        dst_extractors += [
-            jx.PathExtractor(MeasurementStationData._parameter_mapping[data_point],
-                             f"features[*].properties.parameters.{data_point}.data[*]", is_list=True)
-            for data_point in data_points
-        ]
+        for data_point in data_points:
+            reference = local_mapping[data_point]
+            dst_extractors.append(jx.PathExtractor(
+                reference.name, f"features[*].properties.parameters.{data_point}.data[*]",
+                # Use early binding for scale (sc), not default late binding taking the last iteration's value!
+                is_list=True, dst_format=lambda x, sc=reference.scaling: (x * sc if x is not None else None)
+            ))
         return dst_extractors
 
     def fetch_data(self, raw_data: Optional[dict] = None) -> Dict[str, Any]:
@@ -132,10 +193,7 @@ class MeasurementStationData(http_cache.GenericHTTPSourceAPI):
 
         self._logger.debug(f"Try to fetch new readings starting from {self._last_query_ts.isoformat()} to "
                            f"{end_ts.isoformat()}")
-        response: requests.Response = self.session.get(
-            "https://dataset.api.hub.zamg.ac.at/v1/station/historical/klima-v1-10min",
-            params=params
-        )
+        response: requests.Response = self.session.get(self._endpoint_url, params=params)
         response.raise_for_status()
         return response.json()
 
@@ -149,7 +207,8 @@ class MeasurementStationData(http_cache.GenericHTTPSourceAPI):
         """Drops all missing observations and returns the result"""
 
         dropped_keys = []
-        for obs_key in self._parameter_mapping.values():
+        all_ext_keys = {k.name for k in self._local_mapping.values()}
+        for obs_key in all_ext_keys:
             if obs_key in decoded_message and all(map(lambda x: x is None, decoded_message[obs_key])):
                 dropped_keys.append(obs_key)
                 del decoded_message[obs_key]
