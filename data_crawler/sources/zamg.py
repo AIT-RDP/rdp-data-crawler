2ada56835e7fa7f23a469bf98b5ae112ae2ba111
Michael Spiegel <michael.spiegel@ait.ac.at>
2024-05-16 18:19:20 +0200

    feat: Add the initial Geosphere NWP API (#53)
    Further fields need to be computed.

--- data_crawler/sources/zamg.py
+++ data_crawler/sources/zamg.py
@@ -6,6 +6,7 @@ import dataclasses
 import datetime
 import itertools
 import logging
+import math
 from typing import Dict, Any, Optional, List
 
 import data_crawler.access.jsonpath as jx
@@ -65,6 +66,7 @@ class _AbstractGeosphereTimeSeriesAPI(http_cache.GenericHTTPSourceAPI, abc.ABC):
         self.static_request_parameters = {
             "parameters": data_points,
             "output_format": "geojson",  # Choose GeoJSON over CSV to have TZ-Aware timestamps
+            "filename": "measurements",
         }
         self._endpoint_url = self._resolve_endpoint_url(self.endpoint_configs[endpoint_name])
         self._extractors = self._get_extractors(data_points)
@@ -289,7 +291,6 @@ class MeasurementStationData(_AbstractGeosphereTimeSeriesAPI, history.AbstractTi
                                                      **kwargs)
         self.static_request_parameters.update({
             "station_ids": source_parameters["station id"],  # Visible from the web GUI
-            "filename": "measurements",
         })
 
         initial_history = pd.to_timedelta(source_parameters.get("initial history", "48h"))
@@ -358,3 +359,190 @@ class MeasurementStationData(_AbstractGeosphereTimeSeriesAPI, history.AbstractTi
             "end": end_time.isoformat()
         }
         return self.fetch_message(dynamic_request_parameters, raw_data)
+
+
+class NumericalWeatherPredictionData(_AbstractGeosphereTimeSeriesAPI):
+    """
+    Implements the AROME NWP data access.
+
+    The API documentation could be found at https://dataset.api.hub.geosphere.at/v1/docs/user-guide/endpoints.html
+    Meta-data is available at https://dataset.api.hub.geosphere.at/v1/timeseries/forecast/nwp-v1-1h-2500m/metadata
+    """
+
+    _endpoint_configs = {
+        "nwp": _EndpointConfig(parameter_mapping={
+            "cape": _PRef("convective_available_potential_energy"),  # [m2 s-2]
+            "cin": _PRef("convective_inhibition"),  # [J kg-1]
+            "grad": _PRef("_global_horizontal_irradiation_acc"),  # Seems to be an accumulated value. [Ws m-2]
+            "mnt2m": _PRef("air_temperature_min_2m"),  # [degC]
+            "mxt2m": _PRef("air_temperature_max_2m"),  # [degC]
+            "rain_acc": _PRef("_rainfall_mass_total"),  # [kg m-2] Accumulated since start of the forecast
+            "rh2m": _PRef("relative_humidity_2m"),  # [%]
+            "rr_acc": _PRef("_precipitation_mass_total"),  # [kg m-2] Accumulated since start of the forecast
+            "snow_acc": _PRef("snow_surface_mass"),  # [kg m-2] amount on the solid ground
+            "snowlmt": _PRef("snowlimit"),  # [m above ground]
+            "sp": _PRef("air_pressure", scaling=0.01),  # [Pa]
+            "sundur_acc": _PRef("_sunshine_duration_total"),  # [s] sunshine duration since forecast start
+            "t2m": _PRef("air_temperature_2m"),  # [degC]
+            "tcc": _PRef("cloud_area_fraction", scaling=100),  # [1]
+            "u10m": _PRef("_wind_speed_u_10m"),  # [m s-1] wind speed in eastward direction
+            "v10m": _PRef("_wind_speed_v_10m"),  # [m s-1] wind speed in northward direction
+            "ugust": _PRef("_wind_speed_gust_u_10m"),  # [m s-1]
+            "vgust": _PRef("_wind_speed_gust_v_10m"),  # [m s-1]
+        }, type="timeseries", mode="forecast", resource_id="nwp-v1-1h-2500m")
+    }
+
+    def __init__(self, source_parameters, executor_name, **kwargs):
+        """
+        Initializes the forecasting API but does not trigger any query
+
+        :param source_parameters: The source parameters according to the configuration
+        :param executor_name: The name of the executor for debugging purpose
+        :param kwargs: Any extra arguments that will be sent to the super class
+        """
+        super(NumericalWeatherPredictionData, self).__init__(source_parameters=source_parameters,
+                                                             executor_name=executor_name, **kwargs)
+
+        self.static_request_parameters.update({
+            "lat_lon": f"{source_parameters['latitude']},{source_parameters['longitude']}"
+        })
+
+    @property
+    def endpoint_configs(self) -> Dict[str, _EndpointConfig]:
+        return self._endpoint_configs
+
+    def _get_extractors(self, data_points: List[str]) -> List[jx.PathExtractor]:
+        """Overloads the generic extractors by some custom ones"""
+
+        dst_extractors = super()._get_extractors(data_points)
+        dst_extractors += [
+            jx.PathExtractor("forecast_time", "reference_time", is_list=False),
+        ]
+        return dst_extractors
+
+    def fetch_data(self, raw_data: Optional[dict] = None) -> Dict[str, Any]:
+        """
+        Fetches the latest available forecast and returns the result.
+
+        :param raw_data: A geojson string to test the decoding functionality in detail
+        :return: The conventional Redis message
+        """
+
+        out_message = self.fetch_message(raw_data=raw_data)
+        out_message = self._extend_computed_outputs(out_message)
+        return out_message
+
+    def _extend_computed_outputs(self, message: Dict[str, Any]) -> Dict[str, Any]:
+        """Generates the outputs that need further computation."""
+
+        time = [datetime.datetime.fromisoformat(ts) for ts in message["observation_time"]]
+
+        if "_wind_speed_u_10m" in message and "_wind_speed_v_10m" in message:
+            message["wind_speed_10m"], message["wind_direction_10m"] = self._to_abs_dir(
+                message["_wind_speed_u_10m"], message["_wind_speed_v_10m"]
+            )
+
+        if "_global_horizontal_irradiation_acc" in message:
+            message["global_horizontal_irradiation"] = self._to_cnt_derivative(
+                time, message["_global_horizontal_irradiation_acc"], "_global_horizontal_irradiation_acc"
+            )
+
+        if "_rainfall_mass_total" in message and "air_temperature_2m" in message:
+            rain_mass_rate = self._to_cnt_derivative(
+                time, message["_rainfall_mass_total"], "_rainfall_mass_total"
+            )
+            temperature = message["air_temperature_2m"]
+            precipitation_1h = self._to_1h_precipitation_rain(rain_mass_rate, temperature)
+            if precipitation_1h is not None:
+                message["rainfall_total_1h"] = precipitation_1h
+
+        return message
+
+    def _to_1h_precipitation_rain(self, rain_mass_rate: List[float], temperature: List[float]) -> Optional[List[float]]:
+        """Estimates the rain mass rate based on the temperature estimate and tabulated data"""
+
+        try:
+            # Optional dependencies!
+            import pandas as pd
+            import numpy as np
+        except ModuleNotFoundError:
+            return None
+
+        rain_mass_rate = pd.Series(rain_mass_rate)
+        temperature = pd.Series(temperature)
+
+        # temperature [degC] -> density [kg/dm^3], taken from https://www.engineeringtoolbox.com/water-density-specific-weight-d_595.html
+        density_at_saturation_pressure = pd.Series({
+            0.1: 0.9998495,
+            1: 0.9999017,
+            4: 0.9999749,
+            10: 0.9997000,
+            15: 0.9991026,
+            20: 0.9982067,
+            25: 0.9970470,
+            30: 0.9956488,
+            35: 0.9940326,
+            40: 0.9922152,
+            45: 0.99021,
+            50: 0.98804,
+            55: 0.98569,
+            60: 0.98320
+        })
+
+        temperature = temperature.clip(lower=density_at_saturation_pressure.index[0],
+                                       upper=density_at_saturation_pressure.index[-1])
+        density = np.interp(temperature, density_at_saturation_pressure.index, density_at_saturation_pressure)
+        precipitation_1h = rain_mass_rate * 3600 / density
+
+        # Convert to a list of values having None instead of NaN for unification
+        precipitation_1h = list(x if not math.isnan(x) else None for x in precipitation_1h)
+        return precipitation_1h
+
+    @staticmethod
+    def _to_abs_dir(u_values, v_values):
+        """
+        Converts the u/v representation to an abs, dir representation and returns it.
+        :param u_values: wind in eastwards direction (coming from west)
+        :param v_values: wind in northward direction (coming from south)
+        :return: The absolute quantity and the direction in degrees (0° is north, 90° east, etc.)
+        """
+
+        direction = [
+            math.atan2(u, v) * 180.0 / math.pi + 180
+            for u, v in zip(u_values, v_values)
+        ]
+
+        absolute = [
+            math.sqrt(x * x + y * y)
+            for x, y in zip(u_values, v_values)
+        ]
+        return absolute, direction
+
+    def _to_cnt_derivative(self, time: List[datetime.datetime], values: List[float],
+                           measurement: str = "<unknown>") -> List[float]:
+        """
+        Computes the discrete derivative of the given counter.
+
+        Since the first values may not be passed on to the forecast in case it is fetched later on, the counters may not
+        start at zero. To avoid invalid first samples, the first element returned is always None. It is also assumed
+        that the integral of values was performed using the units seconds abd that the counter always counts upwards.
+        Since it is observed that some conters slightly count downwards, any negative derivative will be rounded to zero
+        and added to the next number. to cancel out subsequent faults.
+        """
+        assert len(time) == len(values)
+
+        ret: List[Optional[float]] = [None] if len(time) > 0 else []  # Return an empty array, if no elements are given
+        carry = 0.0
+        for i in range(1, len(time)):
+            dt = (time[i] - time[i - 1]).total_seconds()
+            dv = (values[i] - values[i - 1]) + carry
+
+            if dv < 0.0:
+                self.logger.warning(f"'{measurement}' returned by Geosphere counts downwards by {dv}. Round to 0.0.")
+                carry = dv
+                dv = 0.0
+            else:
+                carry = 0.0
+
+            ret.append(dv / dt)
+        return ret
