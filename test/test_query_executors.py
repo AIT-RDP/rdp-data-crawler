ab04927a8d6564d7f06031deb0378f28fbfd828d
Michael Spiegel <michael.spiegel@ait.ac.at>
2024-06-11 17:33:39 +0200

    refactor: mockup source API name (#48)

--- test/test_query_executors.py
+++ test/test_query_executors.py
@@ -31,7 +31,7 @@ def mockup_service_config(appended_test_path):
     """Returns the configuration of a simple mockup service"""
 
     return {
-        "type": "sources.mockup.MockupSourceAPI",
+        "type": "sources.mockup.PassiveMockupSourceAPI",
         "source parameter": {
             "key": "<keep it secret>",
             "some_list": [1, 2, 4],
@@ -56,7 +56,7 @@ def mockup_service_config(appended_test_path):
 def mockup_service_config_dynamic(appended_test_path):
     """Returns the novel, dynamic sink configuration"""
     return {
-        "type": "sources.mockup.MockupSourceAPI",
+        "type": "sources.mockup.PassiveMockupSourceAPI",
         "source parameter": {
             "key": "<keep it secret>",
             "some_list": [1, 2, 4],
@@ -97,9 +97,9 @@ def test_thread_executor_api_instantiation(mockup_service_config, redis_pool):
     """Tests the API instantiation function using the mockup API"""
 
     executor = query_executors.ThreadQueryExecutor(mockup_service_config, redis_pool)
-    assert isinstance(executor.source_api, mockup.MockupSourceAPI)
+    assert isinstance(executor.source_api, mockup.PassiveMockupSourceAPI)
 
-    api: mockup.MockupSourceAPI = executor.source_api
+    api: mockup.PassiveMockupSourceAPI = executor.source_api
     assert "key" in api.config
     assert api.config["key"] == "<keep it secret>"
 
@@ -111,9 +111,9 @@ def test_thread_executor_api_lifecycle(mockup_service_config, redis_pool):
     """Tests the API instantiation function using the mockup API"""
 
     executor = query_executors.ThreadQueryExecutor(mockup_service_config, redis_pool)
-    assert isinstance(executor.source_api, mockup.MockupSourceAPI)
+    assert isinstance(executor.source_api, mockup.PassiveMockupSourceAPI)
 
-    api: mockup.MockupSourceAPI = executor.source_api
+    api: mockup.PassiveMockupSourceAPI = executor.source_api
     assert api.fetch_invocations == 0
     assert api.start_invocations == 0
     assert api.stop_invocations == 0
@@ -134,7 +134,7 @@ def test_thread_executor_api_status(mockup_service_config, redis_pool):
     """Tests the API instantiation function using the mockup API"""
 
     executor = query_executors.ThreadQueryExecutor(mockup_service_config, redis_pool)
-    assert isinstance(executor.source_api, mockup.MockupSourceAPI)
+    assert isinstance(executor.source_api, mockup.PassiveMockupSourceAPI)
 
     status = executor.get_activity_status()
     assert status.last_wakeup is None
@@ -176,7 +176,7 @@ def test_thread_executor_invalid_api_class(mockup_service_config, redis_pool):
 def test_thread_executor_fetch_invocation(mockup_service_config, redis_pool):
     """Tests whether the API fetch function is correctly invoked"""
 
-    api = mockup.MockupSourceAPI(source_parameters={})
+    api = mockup.PassiveMockupSourceAPI(source_parameters={})
     executor = query_executors.ThreadQueryExecutor(mockup_service_config, redis_pool, source_api=api)
 
     assert api.fetch_invocations == 0
@@ -194,7 +194,7 @@ def test_thread_executor_fetch_invocation(mockup_service_config, redis_pool):
 def test_thread_executor_fetch_error(mockup_service_config, redis_pool):
     """Tests whether the API fetch function is correctly invoked"""
 
-    api = mockup.MockupSourceAPI(source_parameters={"no odd invocations": True})
+    api = mockup.PassiveMockupSourceAPI(source_parameters={"no odd invocations": True})
     executor = query_executors.ThreadQueryExecutor(mockup_service_config, redis_pool, source_api=api)
 
     assert api.fetch_invocations == 0
@@ -212,7 +212,7 @@ def test_thread_executor_fetch_error(mockup_service_config, redis_pool):
 def test_thread_executor_startup_error(mockup_service_config, redis_pool):
     """Tests whether the API fetch function is correctly invoked"""
 
-    api = mockup.MockupSourceAPI(source_parameters={"raise_on_startup": True})
+    api = mockup.PassiveMockupSourceAPI(source_parameters={"raise_on_startup": True})
     executor = query_executors.ThreadQueryExecutor(mockup_service_config, redis_pool, source_api=api)
 
     assert api.fetch_invocations == 0
@@ -229,7 +229,7 @@ def test_thread_executor_redis_export(mockup_service_config, redis_pool, redis_s
     """Tests the executor's capabilities in writing Redis streams"""
 
     mockup_service_config["redis"]["stream"] = redis_stream_name
-    api = mockup.MockupSourceAPI(source_parameters={})
+    api = mockup.PassiveMockupSourceAPI(source_parameters={})
     executor = query_executors.ThreadQueryExecutor(mockup_service_config, redis_pool, source_api=api)
 
     redis_client = redis.Redis(connection_pool=redis_pool)
@@ -263,7 +263,7 @@ def test_thread_executor_redis_templated_export(mockup_service_config, redis_poo
     """Specifically assesses the templated export mechanism"""
 
     mockup_service_config["redis"]["stream"] = redis_stream_name + ".${data}"
-    api = mockup.MockupSourceAPI(source_parameters={})
+    api = mockup.PassiveMockupSourceAPI(source_parameters={})
     executor = query_executors.ThreadQueryExecutor(mockup_service_config, redis_pool, source_api=api)
 
     redis_client = redis.Redis(connection_pool=redis_pool)
@@ -283,7 +283,7 @@ def test_thread_executor_redis_templated_export(mockup_service_config, redis_poo
 def test_thread_executor_sink_dynamic_config(mockup_service_config_dynamic, redis_pool, redis_stream_name):
     """Tests the executor's capabilities in writing Redis streams using metadata"""
 
-    api = mockup.MockupSourceAPI(source_parameters={}, metadata=dict(stream=redis_stream_name))
+    api = mockup.PassiveMockupSourceAPI(source_parameters={}, metadata=dict(stream=redis_stream_name))
     executor = query_executors.ThreadQueryExecutor(mockup_service_config_dynamic, redis_pool, source_api=api)
 
     redis_client = redis.Redis(connection_pool=redis_pool)
@@ -324,10 +324,10 @@ def mockup_executors_config(mockup_service_config):
 
 
 @pytest.fixture()
-def mockup_executors_externals(mockup_executors_config) -> Dict[str, mockup.MockupSourceAPI]:
+def mockup_executors_externals(mockup_executors_config) -> Dict[str, mockup.PassiveMockupSourceAPI]:
     """Instantiates the mockup executors for the collection"""
     return {
-        key: mockup.MockupSourceAPI(config) for key, config in mockup_executors_config.items()
+        key: mockup.PassiveMockupSourceAPI(config) for key, config in mockup_executors_config.items()
     }
 
 
@@ -427,9 +427,9 @@ def test_one_shot_executor_api_instantiation(mockup_service_config, redis_pool):
     """Tests the API instantiation function using the mockup API"""
 
     executor = query_executors.OneShotQueryExecutor(mockup_service_config, redis_pool, name="<test>")
-    assert isinstance(executor.source_api, mockup.MockupSourceAPI)
+    assert isinstance(executor.source_api, mockup.PassiveMockupSourceAPI)
 
-    api: mockup.MockupSourceAPI = executor.source_api
+    api: mockup.PassiveMockupSourceAPI = executor.source_api
     assert "key" in api.config
     assert api.config["key"] == "<keep it secret>"
 
@@ -444,9 +444,9 @@ def test_one_shot_executor_api_lifecycle(mockup_service_config, redis_pool):
     """Tests the API instantiation function using the mockup API"""
 
     executor = query_executors.OneShotQueryExecutor(mockup_service_config, redis_pool, name="<test>")
-    assert isinstance(executor.source_api, mockup.MockupSourceAPI)
+    assert isinstance(executor.source_api, mockup.PassiveMockupSourceAPI)
 
-    api: mockup.MockupSourceAPI = executor.source_api
+    api: mockup.PassiveMockupSourceAPI = executor.source_api
     assert api.fetch_invocations == 0
     assert api.start_invocations == 0
     assert api.stop_invocations == 0
@@ -463,7 +463,7 @@ def test_one_shot_executor_api_lifecycle(mockup_service_config, redis_pool):
 def test_one_shot_executor_fetch_error(mockup_service_config, redis_pool):
     """Tests whether the API fetch function is correctly invoked"""
 
-    api = mockup.MockupSourceAPI(source_parameters={"no odd invocations": True})
+    api = mockup.PassiveMockupSourceAPI(source_parameters={"no odd invocations": True})
     executor = query_executors.OneShotQueryExecutor(mockup_service_config, redis_pool, source_api=api, name="<test>")
 
     assert api.fetch_invocations == 0
@@ -484,7 +484,7 @@ def test_one_shot_executor_redis_export(mockup_service_config, redis_pool, redis
     """Tests the executor's capabilities in writing Redis streams"""
 
     mockup_service_config["redis"]["stream"] = redis_stream_name
-    api = mockup.MockupSourceAPI(source_parameters={})
+    api = mockup.PassiveMockupSourceAPI(source_parameters={})
     executor = query_executors.OneShotQueryExecutor(mockup_service_config, redis_pool, source_api=api, name="test")
 
     redis_client = redis.Redis(connection_pool=redis_pool)
@@ -524,7 +524,7 @@ def test_execute_one_shot_batches(mockup_executors_config, redis_pool):
                                                        debug_return=True)
 
     assert len(sources) == 1
-    api: mockup.MockupSourceAPI = sources["second"]
+    api: mockup.PassiveMockupSourceAPI = sources["second"]
 
     assert api.start_invocations == 1
     assert api.fetch_invocations == 0
@@ -543,7 +543,7 @@ def test_execute_one_shot_batches_override(mockup_executors_config, redis_pool):
                                                        override_config=override, debug_return=True)
 
     assert len(sources) == 1
-    api: mockup.MockupSourceAPI = sources["second"]
+    api: mockup.PassiveMockupSourceAPI = sources["second"]
 
     assert api.config["key"] == "Some new key"
     assert "LetTheHammer" in api.config
