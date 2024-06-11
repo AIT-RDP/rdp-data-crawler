69f6a657ba9a514dc04dceef9c9f467e8a07b200
Michael Spiegel <michael.spiegel@ait.ac.at>
2024-06-11 17:29:41 +0200

    feat: Integrated the active API in the query executors (#48)

--- test/test_query_executors.py
+++ test/test_query_executors.py
@@ -14,87 +14,16 @@ import pytest
 import redis
 
 import data_crawler.query_executors as query_executors
+import data_crawler.sources.abc.active_source_sync as active_source_sync
 import data_crawler.sources.abc.abstract_source as abstract_sources
 import data_crawler.sources.abc.message as msg
 import data_crawler.sources.abc.history as history
 import data_crawler.access.storage as storage
 
+
+import sources.mockup as mockup
+
 logger = logging.getLogger(__name__)
-
-
-class MockupSourceAPI(abstract_sources.AbstractSourceAPI, history.AbstractTimedHistorySourceMixin):
-    """Test API source that just counts function invocations"""
-
-    def __init__(self, source_parameters, metadata: Optional[dict[str, Any]] = None, **kwargs):
-        """Stores the configuration and initializes the object"""
-        self.kwargs = kwargs
-
-        self.config = source_parameters
-        self.fetch_invocations = 0
-        self.history_invocations = 0
-        self.start_invocations = 0
-        self.stop_invocations = 0
-
-        self.fetch_ts = []
-
-        self.enable_fetch = threading.Event()
-        self.enable_fetch.set()
-        self._metadata = metadata
-
-    def start(self):
-        """Counts the start and performs some basic checks"""
-        assert self.start_invocations == self.stop_invocations
-
-        if self.config.get("raise_on_startup", False):
-            raise ValueError("Uups!")
-
-        self.start_invocations += 1
-
-    def fetch_data(self) -> msg.MessageData:
-        """Generates some content and returns it"""
-
-        self.fetch_ts.append(datetime.datetime.utcnow())
-        self.fetch_invocations += 1
-
-        assert self.start_invocations == self.stop_invocations + 1
-
-        if self.config.get("no odd invocations", False) and self.fetch_invocations % 2 == 1:
-            raise ValueError("That's odd.")
-
-        self.enable_fetch.wait()
-
-        payload = {
-            "invocations": self.fetch_invocations,
-            "data": "some-test-nonsense",
-            "duplicate": "api-key"
-        }
-        if self._metadata is not None:
-            return msg.Message(payload=payload, metadata=self._metadata)
-        else:
-            return payload
-
-    def fetch_historic_data(self, start_time: datetime.datetime, end_time: datetime.datetime,
-                            filter_clauses: Dict[str, Any]) -> Dict[str, Any]:
-        """Generates some content and returns it"""
-
-        self.history_invocations += 1
-
-        assert self.start_invocations == self.stop_invocations + 1
-
-        if self.config.get("no odd invocations", False) and self.history_invocations % 2 == 1:
-            raise ValueError("That's odd.")
-
-        return {
-            "fetch_invocations": self.fetch_invocations,
-            "history_invocations": self.history_invocations,
-            "data": "another-test-nonsense",
-            "duplicate": "same-api-key"
-        }
-
-    def stop(self):
-        """Counts the stop and performs some basic checks"""
-        assert self.start_invocations == self.stop_invocations + 1
-        self.stop_invocations += 1
 
 
 @pytest.fixture()
@@ -102,7 +31,7 @@ def mockup_service_config(appended_test_path):
     """Returns the configuration of a simple mockup service"""
 
     return {
-        "type": "test_query_executors.MockupSourceAPI",
+        "type": "sources.mockup.MockupSourceAPI",
         "source parameter": {
             "key": "<keep it secret>",
             "some_list": [1, 2, 4],
@@ -127,7 +56,7 @@ def mockup_service_config(appended_test_path):
 def mockup_service_config_dynamic(appended_test_path):
     """Returns the novel, dynamic sink configuration"""
     return {
-        "type": "test_query_executors.MockupSourceAPI",
+        "type": "sources.mockup.MockupSourceAPI",
         "source parameter": {
             "key": "<keep it secret>",
             "some_list": [1, 2, 4],
@@ -157,9 +86,9 @@ def test_thread_executor_lifecycle(mockup_service_config, redis_pool):
         executor.join()
 
     executor.start()
-    executor.stop()
+    executor.shutdown()
     with pytest.raises(AssertionError):
-        executor.stop()
+        executor.shutdown()
 
     executor.join()
 
@@ -168,9 +97,9 @@ def test_thread_executor_api_instantiation(mockup_service_config, redis_pool):
     """Tests the API instantiation function using the mockup API"""
 
     executor = query_executors.ThreadQueryExecutor(mockup_service_config, redis_pool)
-    assert isinstance(executor.source_api, MockupSourceAPI)
+    assert isinstance(executor.source_api, mockup.MockupSourceAPI)
 
-    api: MockupSourceAPI = executor.source_api
+    api: mockup.MockupSourceAPI = executor.source_api
     assert "key" in api.config
     assert api.config["key"] == "<keep it secret>"
 
@@ -182,9 +111,9 @@ def test_thread_executor_api_lifecycle(mockup_service_config, redis_pool):
     """Tests the API instantiation function using the mockup API"""
 
     executor = query_executors.ThreadQueryExecutor(mockup_service_config, redis_pool)
-    assert isinstance(executor.source_api, MockupSourceAPI)
+    assert isinstance(executor.source_api, mockup.MockupSourceAPI)
 
-    api: MockupSourceAPI = executor.source_api
+    api: mockup.MockupSourceAPI = executor.source_api
     assert api.fetch_invocations == 0
     assert api.start_invocations == 0
     assert api.stop_invocations == 0
@@ -192,7 +121,7 @@ def test_thread_executor_api_lifecycle(mockup_service_config, redis_pool):
 
     executor.start()
     time.sleep(0.6)
-    executor.stop()
+    executor.shutdown()
     executor.join()
 
     assert api.fetch_invocations >= 1
@@ -205,7 +134,7 @@ def test_thread_executor_api_status(mockup_service_config, redis_pool):
     """Tests the API instantiation function using the mockup API"""
 
     executor = query_executors.ThreadQueryExecutor(mockup_service_config, redis_pool)
-    assert isinstance(executor.source_api, MockupSourceAPI)
+    assert isinstance(executor.source_api, mockup.MockupSourceAPI)
 
     status = executor.get_activity_status()
     assert status.last_wakeup is None
@@ -215,7 +144,7 @@ def test_thread_executor_api_status(mockup_service_config, redis_pool):
     ts_start = datetime.datetime.now(tz=datetime.timezone.utc)
     executor.start()
     time.sleep(0.6)
-    executor.stop()
+    executor.shutdown()
     executor.join()
 
     status = executor.get_activity_status()
@@ -247,14 +176,14 @@ def test_thread_executor_invalid_api_class(mockup_service_config, redis_pool):
 def test_thread_executor_fetch_invocation(mockup_service_config, redis_pool):
     """Tests whether the API fetch function is correctly invoked"""
 
-    api = MockupSourceAPI(source_parameters={})
+    api = mockup.MockupSourceAPI(source_parameters={})
     executor = query_executors.ThreadQueryExecutor(mockup_service_config, redis_pool, source_api=api)
 
     assert api.fetch_invocations == 0
 
     executor.start()
     time.sleep(1.25)
-    executor.stop()
+    executor.shutdown()
     executor.join()
 
     assert 2 <= api.fetch_invocations <= 4
@@ -262,80 +191,17 @@ def test_thread_executor_fetch_invocation(mockup_service_config, redis_pool):
             (0.4e6 < api.fetch_ts[-1].microsecond < 0.6e6))  # Check alignment
 
 
-def test_thread_executor_timing_no_force_initial(mockup_service_config, redis_pool):
-    """Tests the timing when no initial sample is forced"""
-
-    mockup_service_config["polling"]["frequency"] = "1s"
-    mockup_service_config["polling"]["force initial"] = False
-
-    api = MockupSourceAPI(source_parameters={})
-    executor = query_executors.ThreadQueryExecutor(mockup_service_config, redis_pool, source_api=api)
-
-    executor.start()
-    time.sleep(2.0)
-    executor.stop()
-    executor.join()
-
-    assert 1 <= api.fetch_invocations <= 3
-    for i, ts in enumerate(api.fetch_ts):
-        assert 0.9e6 < ts.microsecond or ts.microsecond < 0.1e6, f"Alignment error in sample {i}"  # Check alignment
-
-
-def test_thread_executor_timing_force_initial(mockup_service_config, redis_pool):
-    """Tests the timing when the initial sample is forced"""
-
-    mockup_service_config["polling"]["frequency"] = "1s"
-    mockup_service_config["polling"]["force initial"] = True
-
-    api = MockupSourceAPI(source_parameters={})
-    executor = query_executors.ThreadQueryExecutor(mockup_service_config, redis_pool, source_api=api)
-
-    executor.start()
-    ts_now = datetime.datetime.utcnow()
-    time.sleep(2.0)
-    executor.stop()
-    executor.join()
-
-    assert 2 <= api.fetch_invocations <= 3
-    assert -0.1 <= (api.fetch_ts[0] - ts_now).total_seconds() <= 0.1  # The first sample must be triggered immediately
-    for i, ts in enumerate(api.fetch_ts[1:]):
-        assert 0.9e6 < ts.microsecond or ts.microsecond < 0.1e6, f"Alignment error in sample {i}"  # Check alignment
-
-
-def test_thread_executor_slot_mechanism(mockup_service_config, redis_pool):
-    """Tests whether the API fetch function is correctly invoked"""
-
-    mockup_service_config["polling"]["frequency"] = "1s"
-    mockup_service_config["polling"]["slot count"] = 2
-    mockup_service_config["polling"]["slot id"] = "1"  # The function must also support string inputs
-    mockup_service_config["polling"]["force initial"] = False
-
-    api = MockupSourceAPI(source_parameters={})
-    executor = query_executors.ThreadQueryExecutor(mockup_service_config, redis_pool, source_api=api)
-
-    assert api.fetch_invocations == 0
-
-    executor.start()
-    time.sleep(2.0)
-    executor.stop()
-    executor.join()
-
-    assert 1 <= api.fetch_invocations <= 3
-    for i, ts in enumerate(api.fetch_ts):
-        assert 0.4e6 < ts.microsecond < 0.6e6, f"Alignment error in sample {i}"  # Check alignment
-
-
 def test_thread_executor_fetch_error(mockup_service_config, redis_pool):
     """Tests whether the API fetch function is correctly invoked"""
 
-    api = MockupSourceAPI(source_parameters={"no odd invocations": True})
+    api = mockup.MockupSourceAPI(source_parameters={"no odd invocations": True})
     executor = query_executors.ThreadQueryExecutor(mockup_service_config, redis_pool, source_api=api)
 
     assert api.fetch_invocations == 0
 
     executor.start()
     time.sleep(1.25)
-    executor.stop()
+    executor.shutdown()
     executor.join()
 
     assert 2 <= api.fetch_invocations <= 4
@@ -346,7 +212,7 @@ def test_thread_executor_fetch_error(mockup_service_config, redis_pool):
 def test_thread_executor_startup_error(mockup_service_config, redis_pool):
     """Tests whether the API fetch function is correctly invoked"""
 
-    api = MockupSourceAPI(source_parameters={"raise_on_startup": True})
+    api = mockup.MockupSourceAPI(source_parameters={"raise_on_startup": True})
     executor = query_executors.ThreadQueryExecutor(mockup_service_config, redis_pool, source_api=api)
 
     assert api.fetch_invocations == 0
@@ -355,7 +221,7 @@ def test_thread_executor_startup_error(mockup_service_config, redis_pool):
     executor.start()
     assert not executor.is_alive(), "Crash on startup expected"
 
-    executor.stop()
+    executor.shutdown()
     executor.join()
 
 
@@ -363,14 +229,14 @@ def test_thread_executor_redis_export(mockup_service_config, redis_pool, redis_s
     """Tests the executor's capabilities in writing Redis streams"""
 
     mockup_service_config["redis"]["stream"] = redis_stream_name
-    api = MockupSourceAPI(source_parameters={})
+    api = mockup.MockupSourceAPI(source_parameters={})
     executor = query_executors.ThreadQueryExecutor(mockup_service_config, redis_pool, source_api=api)
 
     redis_client = redis.Redis(connection_pool=redis_pool)
 
     executor.start()
     time.sleep(0.51)
-    executor.stop()
+    executor.shutdown()
     executor.join()
 
     messages = redis_client.xrange(redis_stream_name)
@@ -397,14 +263,14 @@ def test_thread_executor_redis_templated_export(mockup_service_config, redis_poo
     """Specifically assesses the templated export mechanism"""
 
     mockup_service_config["redis"]["stream"] = redis_stream_name + ".${data}"
-    api = MockupSourceAPI(source_parameters={})
+    api = mockup.MockupSourceAPI(source_parameters={})
     executor = query_executors.ThreadQueryExecutor(mockup_service_config, redis_pool, source_api=api)
 
     redis_client = redis.Redis(connection_pool=redis_pool)
 
     executor.start()
     time.sleep(0.51)
-    executor.stop()
+    executor.shutdown()
     executor.join()
 
     messages = redis_client.xrange(f"{redis_stream_name}.some-test-nonsense")
@@ -417,14 +283,14 @@ def test_thread_executor_redis_templated_export(mockup_service_config, redis_poo
 def test_thread_executor_sink_dynamic_config(mockup_service_config_dynamic, redis_pool, redis_stream_name):
     """Tests the executor's capabilities in writing Redis streams using metadata"""
 
-    api = MockupSourceAPI(source_parameters={}, metadata=dict(stream=redis_stream_name))
+    api = mockup.MockupSourceAPI(source_parameters={}, metadata=dict(stream=redis_stream_name))
     executor = query_executors.ThreadQueryExecutor(mockup_service_config_dynamic, redis_pool, source_api=api)
 
     redis_client = redis.Redis(connection_pool=redis_pool)
 
     executor.start()
     time.sleep(0.51)
-    executor.stop()
+    executor.shutdown()
     executor.join()
 
     messages = redis_client.xrange(redis_stream_name)
@@ -458,10 +324,10 @@ def mockup_executors_config(mockup_service_config):
 
 
 @pytest.fixture()
-def mockup_executors_externals(mockup_executors_config) -> Dict[str, MockupSourceAPI]:
+def mockup_executors_externals(mockup_executors_config) -> Dict[str, mockup.MockupSourceAPI]:
     """Instantiates the mockup executors for the collection"""
     return {
-        key: MockupSourceAPI(config) for key, config in mockup_executors_config.items()
+        key: mockup.MockupSourceAPI(config) for key, config in mockup_executors_config.items()
     }
 
 
@@ -561,9 +427,9 @@ def test_one_shot_executor_api_instantiation(mockup_service_config, redis_pool):
     """Tests the API instantiation function using the mockup API"""
 
     executor = query_executors.OneShotQueryExecutor(mockup_service_config, redis_pool, name="<test>")
-    assert isinstance(executor.source_api, MockupSourceAPI)
+    assert isinstance(executor.source_api, mockup.MockupSourceAPI)
 
-    api: MockupSourceAPI = executor.source_api
+    api: mockup.MockupSourceAPI = executor.source_api
     assert "key" in api.config
     assert api.config["key"] == "<keep it secret>"
 
@@ -578,9 +444,9 @@ def test_one_shot_executor_api_lifecycle(mockup_service_config, redis_pool):
     """Tests the API instantiation function using the mockup API"""
 
     executor = query_executors.OneShotQueryExecutor(mockup_service_config, redis_pool, name="<test>")
-    assert isinstance(executor.source_api, MockupSourceAPI)
+    assert isinstance(executor.source_api, mockup.MockupSourceAPI)
 
-    api: MockupSourceAPI = executor.source_api
+    api: mockup.MockupSourceAPI = executor.source_api
     assert api.fetch_invocations == 0
     assert api.start_invocations == 0
     assert api.stop_invocations == 0
@@ -597,7 +463,7 @@ def test_one_shot_executor_api_lifecycle(mockup_service_config, redis_pool):
 def test_one_shot_executor_fetch_error(mockup_service_config, redis_pool):
     """Tests whether the API fetch function is correctly invoked"""
 
-    api = MockupSourceAPI(source_parameters={"no odd invocations": True})
+    api = mockup.MockupSourceAPI(source_parameters={"no odd invocations": True})
     executor = query_executors.OneShotQueryExecutor(mockup_service_config, redis_pool, source_api=api, name="<test>")
 
     assert api.fetch_invocations == 0
@@ -618,7 +484,7 @@ def test_one_shot_executor_redis_export(mockup_service_config, redis_pool, redis
     """Tests the executor's capabilities in writing Redis streams"""
 
     mockup_service_config["redis"]["stream"] = redis_stream_name
-    api = MockupSourceAPI(source_parameters={})
+    api = mockup.MockupSourceAPI(source_parameters={})
     executor = query_executors.OneShotQueryExecutor(mockup_service_config, redis_pool, source_api=api, name="test")
 
     redis_client = redis.Redis(connection_pool=redis_pool)
@@ -658,7 +524,7 @@ def test_execute_one_shot_batches(mockup_executors_config, redis_pool):
                                                        debug_return=True)
 
     assert len(sources) == 1
-    api: MockupSourceAPI = sources["second"]
+    api: mockup.MockupSourceAPI = sources["second"]
 
     assert api.start_invocations == 1
     assert api.fetch_invocations == 0
@@ -677,7 +543,7 @@ def test_execute_one_shot_batches_override(mockup_executors_config, redis_pool):
                                                        override_config=override, debug_return=True)
 
     assert len(sources) == 1
-    api: MockupSourceAPI = sources["second"]
+    api: mockup.MockupSourceAPI = sources["second"]
 
     assert api.config["key"] == "Some new key"
     assert "LetTheHammer" in api.config
