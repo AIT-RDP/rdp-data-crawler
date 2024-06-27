e47b79418d4ec03ebdf4818d9b3ca0b529523ded
Michael Spiegel <michael.spiegel@ait.ac.at>
2024-06-27 17:04:17 +0200

    feat: Extend the supervisor tests (#50, #58)
    Include an active source as well

--- test/test_query_supervisor.py
+++ test/test_query_supervisor.py
@@ -5,7 +5,7 @@ Implements the test cases for the query supervisor
 import asyncio
 import copy
 import logging
-from typing import Dict
+from typing import Dict, Union
 
 import pyrdp_commons as commons
 import pytest
@@ -16,8 +16,8 @@ import sources.mockup as mockup
 
 
 @pytest.fixture()
-def static_mockup_service_config(appended_test_path):
-    """Returns the configuration of a simple mockup service"""
+def passive_mockup_service_config(appended_test_path):
+    """Returns the configuration of a simple mockup service using the passive interface"""
 
     return {
         "type": "sources.mockup.PassiveMockupSourceAPI",
@@ -42,11 +42,35 @@ def static_mockup_service_config(appended_test_path):
 
 
 @pytest.fixture()
-def mockup_executors_config(static_mockup_service_config):
+def active_mockup_service_config(appended_test_path):
+    """Returns the configuration of a simple mockup service following the active interface"""
+
+    return {
+        "type": "sources.mockup.ActiveMockupSourceAPI",
+        "source parameter": {
+            "sleep_time": 0.5,
+            "repeat": True,
+            "messages": [{}]
+        },
+        "redis": {
+            "stream": "my-stream",
+            "tags": {
+                "source type": "mockup-test",
+                "empty": "",
+                "my-number": 0.2,
+                "duplicate": "config-key"
+            },
+        },
+    }
+
+
+@pytest.fixture()
+def mockup_executors_config(passive_mockup_service_config, active_mockup_service_config):
     """The mockup configuration without any dynamic elements"""
     return {
-        "first": copy.deepcopy(static_mockup_service_config),
-        "second": copy.deepcopy(static_mockup_service_config)
+        "first": copy.deepcopy(passive_mockup_service_config),
+        "second": copy.deepcopy(passive_mockup_service_config),
+        "third": copy.deepcopy(active_mockup_service_config)
     }
 
 
@@ -57,10 +81,16 @@ async def mockup_executors_config_container(mockup_executors_config) -> commons.
 
 
 @pytest.fixture()
-def mockup_executors_externals(mockup_executors_config) -> Dict[str, mockup.PassiveMockupSourceAPI]:
+def mockup_executors_externals(
+        mockup_executors_config
+) -> Dict[str, Union[mockup.PassiveMockupSourceAPI, mockup.ActiveMockupSourceAPI]]:
     """Instantiates the mockup executors for the collection"""
     return {
-        key: mockup.PassiveMockupSourceAPI(config) for key, config in mockup_executors_config.items()
+        "first": mockup.PassiveMockupSourceAPI(mockup_executors_config["first"]["source parameter"]),
+        "second": mockup.PassiveMockupSourceAPI(mockup_executors_config["second"]["source parameter"]),
+        "third": mockup.ActiveMockupSourceAPI.create(
+            mockup.ActiveMockupSourceParameters.model_validate(mockup_executors_config["second"]["source parameter"])
+        )
     }
 
 
@@ -74,11 +104,11 @@ async def test_query_supervisor_life_cycle(mockup_executors_config_container, mo
     await supervisor.start()
 
     status = await supervisor.heartbeat()
-    assert status == {"first": "ok", "second": "ok"}
+    assert status == {"first": "ok", "second": "ok", "third": "ok"}
 
     await asyncio.sleep(0.6)
     status = await supervisor.heartbeat()
-    assert status == {"first": "ok", "second": "ok"}
+    assert status == {"first": "ok", "second": "ok", "third": "ok"}
 
     assert mockup_executors_externals["first"].start_invocations == 1
     assert mockup_executors_externals["first"].fetch_invocations >= 1
@@ -88,6 +118,12 @@ async def test_query_supervisor_life_cycle(mockup_executors_config_container, mo
     assert mockup_executors_externals["second"].fetch_invocations >= 1
     assert mockup_executors_externals["second"].stop_invocations == 0
 
+    assert len(mockup_executors_externals["third"].start_exec) == 1
+    assert len(mockup_executors_externals["third"].run_exec) == 1
+    assert len(mockup_executors_externals["third"].run_message_exec) >= 1
+    assert len(mockup_executors_externals["third"].shutdown_exec) == 0
+    assert len(mockup_executors_externals["third"].stop_exec) == 0
+
     await supervisor.stop()
     assert mockup_executors_externals["first"].start_invocations == 1
     assert mockup_executors_externals["first"].fetch_invocations >= 1
@@ -96,6 +132,12 @@ async def test_query_supervisor_life_cycle(mockup_executors_config_container, mo
     assert mockup_executors_externals["second"].start_invocations == 1
     assert mockup_executors_externals["second"].fetch_invocations >= 1
     assert mockup_executors_externals["second"].stop_invocations == 1
+
+    assert len(mockup_executors_externals["third"].start_exec) == 1
+    assert len(mockup_executors_externals["third"].run_exec) == 1
+    assert len(mockup_executors_externals["third"].run_message_exec) >= 1
+    assert len(mockup_executors_externals["third"].shutdown_exec) == 1
+    assert len(mockup_executors_externals["third"].stop_exec) == 1
 
 
 @pytest.mark.asyncio
@@ -110,26 +152,26 @@ async def test_query_supervisor_restart(mockup_executors_config_container, mocku
     await supervisor.start()
 
     status = await supervisor.heartbeat()
-    assert status == {"first": "ok", "second": "ok"}
+    assert status == {"first": "ok", "second": "ok", "third": "ok"}
 
     mockup_executors_externals["second"].enable_fetch.clear()  # Block the execution
     await asyncio.sleep(1.1)
 
     status = await supervisor.heartbeat()
-    assert status == {"first": "ok", "second": "stop-by-timeout"}
+    assert status == {"first": "ok", "second": "stop-by-timeout", "third": "ok"}
     status = await supervisor.heartbeat()
-    assert status == {"first": "ok", "second": "blocking"}
+    assert status == {"first": "ok", "second": "blocking", "third": "ok"}
 
     mockup_executors_externals["second"].enable_fetch.set()
     await asyncio.sleep(0.1)  # Give the second source some time to terminate
 
     status = await supervisor.heartbeat()
-    assert status == {"first": "ok", "second": "restarted"}
+    assert status == {"first": "ok", "second": "restarted", "third": "ok"}
 
     await asyncio.sleep(0.6)
 
     status = await supervisor.heartbeat()
-    assert status == {"first": "ok", "second": "ok"}
+    assert status == {"first": "ok", "second": "ok", "third": "ok"}
 
     await supervisor.stop()
 
