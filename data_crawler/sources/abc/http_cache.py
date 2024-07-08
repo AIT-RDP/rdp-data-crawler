7427901b03f0873153c002a07478be95ed172c9b
Michael Spiegel <michael.spiegel@ait.ac.at>
2024-07-08 16:42:35 +0200

    refactor: Refactors the caching mechanism into a mixin (#63, #62)
    In addition, the testing duration is prolonged to fix #62

--- data_crawler/sources/abc/http_cache.py
+++ data_crawler/sources/abc/http_cache.py
@@ -13,21 +13,27 @@ import requests
 import data_crawler.sources.abc.abstract_source as abstract_source
 
 
-class GenericHTTPSourceAPI(abstract_source.AbstractSourceAPI, abc.ABC):
+class SyncHTTPMixin(abc.ABC):
     """
-    Provides an HTTP session member that caches local calls according to the caching configuration
+    Generic mixin class that can be used to include HTTP caching functionality
+
+    The class is intended to provide caching to arbitrary synchronous sources. Just use the class as the first parent
+    and make sure that the source_parameters keyword argument is given. It will relay all other arguments automatically
+    to the other parents.
     """
 
-    def __init__(self, source_parameters: dict, **kwargs):
+    def __init__(self, *args, **kwargs):
         """
         Initializes the cache
 
-        :param source_parameters: The source configuration including a cache section
-        :param kwargs: Any additional parameters to maintain compatibility
+        :param args: Positional arguments to be relayed to the other parent classes
+        :param kwargs: Keyword arguments to be relayed to the other parent classes. The keyword arguments must include
+            the source_parameters keyword.
         """
+        super().__init__(*args, **kwargs)
+        assert "source_parameters" in kwargs, "Expect the 'source_parameters' keyword"
 
-        super(GenericHTTPSourceAPI, self).__init__(source_parameters=source_parameters, **kwargs)
-
+        source_parameters = kwargs["source_parameters"]
         config = source_parameters.get("cache", {})
 
         req_session = requests.Session()
@@ -40,3 +46,22 @@ class GenericHTTPSourceAPI(abstract_source.AbstractSourceAPI, abc.ABC):
             heuristic = None
 
         self.session = cachecontrol.CacheControl(req_session, cache, heuristic=heuristic)
+
+
+class GenericHTTPSourceAPI(SyncHTTPMixin, abstract_source.AbstractSourceAPI, abc.ABC):
+    """
+    Provides an HTTP session member that caches local calls according to the caching configuration
+
+    The class is provided for compatibility reasons and can be seen as deprecated. Please use the appropriate
+    SyncHTTPMixin mixin instead.
+    """
+
+    def __init__(self, source_parameters: dict, **kwargs):
+        """
+        Initializes the cache
+
+        :param source_parameters: The source configuration including a cache section
+        :param kwargs: Any additional parameters to maintain compatibility
+        """
+
+        super(GenericHTTPSourceAPI, self).__init__(source_parameters=source_parameters, **kwargs)
--- test/test_http_cache.py
+++ test/test_http_cache.py
@@ -4,12 +4,13 @@ Tests the HTTP caching mechanism provided by the GenericHTTPSourceAPI class
 import os
 import shutil
 import time
-from typing import Dict, Any
+from typing import Dict, Any, Generator
 
 import pandas as pd
 import requests
 
 import data_crawler.sources.abc.http_cache as http_cache
+import data_crawler.sources.abc.abstract_source as abstract_source
 
 
 class TimeSourceAPI(http_cache.GenericHTTPSourceAPI):
@@ -47,13 +48,13 @@ def test_expiration_date():
     api = TimeSourceAPI(source_parameters={
         "cache": {
             "directory": ".cache-test",
-            "expire": "1s"
+            "expire": "2s"
         },
     })
 
     call_0 = api.fetch_data()
     call_1 = api.fetch_data()
-    time.sleep(2.0)
+    time.sleep(3.0)
     call_2 = api.fetch_data()
 
     assert os.path.isdir(".cache-test")
@@ -72,7 +73,7 @@ def test_expiration_default():
     api = TimeSourceAPI(source_parameters={
         "cache": {
             "directory": ".cache-test",
-            "expire": "1s"
+            "expire": "2s"
         },
     })
 
@@ -85,3 +86,85 @@ def test_expiration_default():
     assert call_0["header date"] == call_1["header date"]
     assert call_0["last update"] == call_1["last update"]
     assert call_0["header expires"] == call_1["header expires"]
+
+
+class TimeSourceMixinAPI(http_cache.SyncHTTPMixin, abstract_source.AbstractMultiMessageSourceAPI):
+    """Just queries the current time of day from a status API. Caches the result using the Mixin pattern"""
+
+    def __init__(self, **kwargs):
+        """
+        Initializes the source API
+
+        :param kwargs: Parameters passed on to the underlying GenericHTTPSourceAPI
+        """
+
+        super(TimeSourceMixinAPI, self).__init__(**kwargs)
+        if "DATA_CRAWLER_CONTACT" not in os.environ:
+            raise KeyError("Please put your contact details in the environment variable DATA_CRAWLER_CONTACT.")
+        self.session.headers["User-Agent"] = f"E3SchoolEMS_Test_Suite {os.environ['DATA_CRAWLER_CONTACT']}"
+
+    def fetch_data_bundle(self) -> Generator[abstract_source.MessageData, None, None]:
+        """Queries the status API and returns the result"""
+
+        response: requests.Response = self.session.get("https://api.met.no/weatherapi/locationforecast/2.0/status.json")
+        response.raise_for_status()
+
+        status = response.json()
+        yield {
+            "last update": pd.to_datetime(status["last_update"]),
+            "header date": pd.to_datetime(response.headers["date"]),
+            "header expires": pd.to_datetime(response.headers["expires"])
+        }
+
+
+def test_expiration_date_mixin():
+    """Tests the caching mechanism using an expiration date and the mixin pattern"""
+
+    api = TimeSourceMixinAPI(source_parameters={
+        "cache": {
+            "directory": ".cache-test",
+            "expire": "2s"
+        },
+    })
+
+    call_0 = list(api.fetch_data_bundle())
+    call_1 = list(api.fetch_data_bundle())
+    time.sleep(3.0)
+    call_2 = list(api.fetch_data_bundle())
+
+    assert os.path.isdir(".cache-test")
+    shutil.rmtree(".cache-test")
+
+    assert len(call_0) == 1
+    assert len(call_1) == 1
+    assert len(call_2) == 1
+
+    assert call_0[0]["header date"] == call_1[0]["header date"]
+    assert call_0[0]["header date"] < call_2[0]["header date"]
+
+    assert call_0[0]["last update"] == call_1[0]["last update"]
+    assert call_0[0]["header expires"] == call_1[0]["header expires"]
+
+
+def test_expiration_mixin_default():
+    """Tests the default expiration behaviour according to the external website using the mixin pattern"""
+
+    api = TimeSourceAPI(source_parameters={
+        "cache": {
+            "directory": ".cache-test",
+            "expire": "2s"
+        },
+    })
+
+    call_0 = list(api.fetch_data_bundle())
+    call_1 = list(api.fetch_data_bundle())
+
+    assert os.path.isdir(".cache-test")
+    shutil.rmtree(".cache-test")
+
+    assert len(call_0) == 1
+    assert len(call_1) == 1
+
+    assert call_0[0]["header date"] == call_1[0]["header date"]
+    assert call_0[0]["last update"] == call_1[0]["last update"]
+    assert call_0[0]["header expires"] == call_1[0]["header expires"]
