f1560573163fe8bbc7e988fdba21b8184c1813e6
Michael Spiegel <michael.spiegel@ait.ac.at>
2025-02-24 20:22:26 +0100

    feat: Enables type support for the path extractors (#89)

--- test/access/test_jsonpath.py
+++ test/access/test_jsonpath.py
@@ -58,6 +58,22 @@ def test_path_extractor(json_data, path: str, is_list: bool, reference_message):
     assert result["test"] == reference_message
 
 
+@pytest.mark.parametrize("in_data, target_type", [
+    (12, float), (True, float), ("13", float), (14.5, float),
+    (15, int), (16.1, int), ("17", int),
+    (1, bool), (True, bool), (0, bool)
+])
+def test_path_extractor_target_type(in_data, target_type):
+    """Tests whether the path extractor successfully converts the types to the target data type"""
+
+    extractor = jx.PathExtractor("test", "d", is_list=False, target_type=target_type)
+
+    result = extractor.extract_information({"d": in_data})
+    result = result["test"]
+    assert result == target_type(in_data)
+    assert isinstance(result, target_type)
+
+
 @pytest.mark.parametrize("path,is_list,reference_message", [
     ("meta.'query date'", False, "2022-08-10T18:00:00+00:00"),
     ("samples[*].time", True, ["2022-08-10T12:00:00+00:00", "2022-08-10T13:00:00+00:00", "2022-08-10T14:00:00+00:00"])
@@ -92,3 +108,27 @@ def test_optional_path_extractor(json_data):
     result = extractor.extract_information(json_data)
     assert "test" in result
     assert result["test"] == [0, 1, 0]
+
+
+@pytest.mark.parametrize("in_data, target_type, use_default", [
+    (12, float, True), (12, float, False),
+    (16.1, int, True), (16.1, int, False),
+    (1, bool, True), (1, bool, False)
+])
+def test_path_optional_path_extractor_target_type(in_data, target_type, use_default: bool):
+    """Tests whether the optional path extractor successfully converts the types to the target data type"""
+
+    extractor = jx.OptionalPathExtractor("test", "d", "e", is_list=False,
+                                         default_value=in_data * use_default,
+                                         target_type=target_type)
+
+    if use_default:
+        src_data = {"d": {}}
+    else:
+        src_data = {"d": {"e": in_data}}
+
+    result = extractor.extract_information(src_data)
+    result = result["test"]
+
+    assert result == target_type(in_data)
+    assert isinstance(result, target_type)
