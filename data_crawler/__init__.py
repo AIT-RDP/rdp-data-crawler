ba3725786e656cb1e5387f3a4e9d6bdfe089864e
Michael Spiegel <michael.spiegel@ait.ac.at>
2026-08-04 19:30:05 +0200

    feat: Upgrade from poetry to uv (#114)

--- data_crawler/__init__.py
+++ data_crawler/__init__.py
@@ -1,3 +1,7 @@
 
-from . import _version
-__version__ = _version.get_versions()['version']
+from importlib.metadata import PackageNotFoundError, version as _package_version
+
+try:
+    __version__ = _package_version("rdp-data-crawler")
+except PackageNotFoundError:  # The package metadata is unavailable, e.g. when running from a plain source checkout
+    __version__ = "0+unknown"
